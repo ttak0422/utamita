5c1b19c13214da661f08081024370c0eebf72cc2
ttak0422 <bgdaewalkman@gmail.com>
2020-10-01 19:27:25 +0900

    :bug: fix 初期値の読み込みを行うように

--- src/eventpage.ts
+++ src/eventpage.ts
@@ -4,6 +4,7 @@
  */
 
 import { keys, appEvent } from "./util";
+import { chromeGet } from "./chrome";
 
 const chromeTabsEvents = {
     unloaded: "unloaded",
@@ -14,6 +15,16 @@ const chromeTabsEvents = {
 let enabled = true;
 let watchList: number[] = [];
 
+chromeGet(keys.enabled).then(savedEnabled => {
+    if (savedEnabled !== undefined) {
+        console.log(`load enabled: ${savedEnabled}`);
+        enabled = savedEnabled as boolean;
+        for (let tabId of watchList) {
+            chrome.tabs.sendMessage(tabId, { type: enabled ? appEvent.on : appEvent.off }, () => { });
+        }
+    }
+});
+
 chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
     // URLがYouTubeの視聴ページだった場合，`content_scripts`経由でスクリプトを注入する．
     if (info.status === chromeTabsEvents.complete && tab.url?.indexOf("https://www.youtube.com/watch?") !== -1) {
@@ -23,7 +34,6 @@ chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
             watchList.push(tabId);
             // 最速で反映される方法で一旦ミュート
             chrome.tabs.executeScript(tabId, { code: 'let v=document.getElementsByClassName("video-stream html5-main-video")[0];if(v!==null)v.volume=0;' }, () => { });
-            chrome.tabs.sendMessage(tabId, { type: "onUpdate" }, () => { });
             chrome.tabs.sendMessage(tabId, { type: appEvent.on }, () => { });
         }
     }
